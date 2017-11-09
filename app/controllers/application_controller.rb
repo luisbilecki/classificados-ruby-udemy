391ce49e5d8c20f80ab19c1ce32082770b9e5db9
luis.bilecki <luis.bilecki@gmail.com>
2017-11-09 13:42:11 +0000

    Aula 20 - Ajuste nos forms do devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Only the application can access the methods
+  protected
+
+  # Layout per resource_name
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "backoffice_devise"
+    else
+      "application"
+    end
+  end
+
 end
