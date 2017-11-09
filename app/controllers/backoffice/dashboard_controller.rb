5ad0f0f76eac9f5df1f62bad4fcd8d279ddf439f
luis.bilecki <luis.bilecki@gmail.com>
2017-11-09 13:00:57 +0000

    Aula 18 - Ajustes no Layout e Categorias

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Backoffice::DashboardController < ApplicationController
+  #Antes de qualquer ação pede a autenticação
+  before_action :authenticate_admin!
+
   layout "backoffice"
 
   def index
