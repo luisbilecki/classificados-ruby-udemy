aae0f9463190daad656988330da1c17a3bf91d9e
luis.bilecki <luis.bilecki@gmail.com>
2017-11-14 11:43:22 +0000

    Aula 32 - Active Record Enum

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,18 @@
 class Admin < ActiveRecord::Base
+  enum role: [ :full_access, :restricted_access ]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  #Transforma enum em string
+  def role_br
+    if self.role == 'full_access'
+      'Acesso Completo'
+    else
+      'Acesso Restrito'
+    end
+  end
 end
