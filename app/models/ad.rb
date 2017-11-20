8c559084bf57d263e4c5d74d113cd58504a84c57
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-11-20 16:06:47 -0200

    Aula 69 -Adicionando data para o anúncio

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,7 +3,9 @@ class Ad < ActiveRecord::Base
   belongs_to :member
 
   #validates
-  validates_presence_of :title, :description, :category, :price, :picture
+  validates :title, :description, :category, :picture, presence: true
+  validates :finish_date, presence: true
+  validates :price , numericality: { greater_than: 0 }
 
   # paperclip
   has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100>" },
