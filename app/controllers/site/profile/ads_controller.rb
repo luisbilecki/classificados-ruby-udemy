f5c476e695ca720b41d9712962830a79cd70e86e
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-11-20 15:11:57 -0200

    Aula 65 e 66 - Editando anúncios

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,17 @@
 class Site::Profile::AdsController < Site::ProfileController
+  before_action :set_ad, only: [:edit, :update]
+
   def index
     @ads = Ad.ads_to_cm(current_member)
+  end
+
+  def edit
+    # set ad via before action
+  end
+
+  private
+
+  def set_ad
+    @ad = Ad.find(params[:id])
   end
 end
