a85894d8265abd43e96739d44074c64182d639e3
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-11-20 15:23:05 -0200

    Aula 67 - Atualizando os anúncios

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -9,9 +9,21 @@ class Site::Profile::AdsController < Site::ProfileController
     # set ad via before action
   end
 
+  def update
+    if @ad.update(params_ad)
+      redirect_to site_profile_ads_path, notice: "Anúncio atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_ad
     @ad = Ad.find(params[:id])
+  end
+
+  def params_ad
+    params.require(:ad).permit(:id, :title, :category_id, :price, :description, :picture)
   end
 end
